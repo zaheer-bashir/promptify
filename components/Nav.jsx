bf711aeb4d70c6206f59d059d4cb729cc14fd4d5
Zaheer Bashir <zaheerkpr3@gmail.com>
2024-06-01 23:58:28 +0500

    Layout structure set

---
+++ components/Nav.jsx
@@ -0,0 +1,17 @@
+"use client"
+import Link from "next/link"
+import Image from "next/image"
+import { useState, useEffect } from "react"
+import { signIn , signOut, useSession , getProviders } from "next-auth/react"
+
+const Nav = () => {
+  return (
+    <nav className="flex-between w-full mb-16 pt-3">
+        <Link href="/" className="flex gap-2 flex-center">
+            <Image src="/assets/images/logo.svg" alt="Promptify" width={40} height={40} className="object-contain" />
+        </Link>
+    </nav>
+  )
+}
+
+export default Nav
