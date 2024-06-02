16d1711fcd6c93618fe0980f143c6d977199a171
Zaheer Bashir <zaheerkpr3@gmail.com>
2024-06-02 22:48:59 +0500

    Next Auth Google Authentication and MongoDB connection

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,28 @@
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+
+console.log(
+    process.env.GOOGLE_CLIENT_ID
+);
+
+console.log(
+    process.env.GOOGLE_CLIENT_SECRET
+);
+
+const handler = NextAuth({
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET
+        })
+    ],
+
+    async session({ session }) {
+       
+    },
+    async signIn({profile}){
+
+    }
+});
+
+export  { handler as GET, handler as POST};
